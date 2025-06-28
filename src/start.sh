6596ed26cf228c463df1cf5df75d4cb3840288fd
RajinZajin <129674267+rajinzajin@users.noreply.github.com>
2025-06-28 21:16:04 +0100

    Update start.sh

--- src/start.sh
+++ src/start.sh
@@ -5,7 +5,7 @@ TCMALLOC="$(ldconfig -p | grep -Po "libtcmalloc.so.\d" | head -n 1)"
 export LD_PRELOAD="${TCMALLOC}"
 
 # Ensure ComfyUI-Manager runs in offline network mode inside the container
-comfy-manager-set-mode offline || echo "worker-comfyui - Could not set ComfyUI-Manager network_mode" >&2
+#comfy-manager-set-mode offline || echo "worker-comfyui - Could not set ComfyUI-Manager network_mode" >&2
 
 echo "worker-comfyui: Starting ComfyUI"
 
