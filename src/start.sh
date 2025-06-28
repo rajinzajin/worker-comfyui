b9d618e38436d1c830731b2f29bb06ccf5dc7222
RajinZajin <129674267+rajinzajin@users.noreply.github.com>
2025-06-28 21:15:39 +0100

    Update start.sh

--- src/start.sh
+++ src/start.sh
@@ -14,12 +14,12 @@ echo "worker-comfyui: Starting ComfyUI"
 
 # Serve the API and don't shutdown the container
 if [ "$SERVE_API_LOCALLY" == "true" ]; then
-    python -u /comfyui/main.py --disable-auto-launch --disable-metadata --listen --verbose "${COMFY_LOG_LEVEL}" --log-stdout &
+    python /comfyui/main.py
 
     # echo "worker-comfyui: Starting RunPod Handler"
     # python -u /handler.py --rp_serve_api --rp_api_host=0.0.0.0
 else
-    python -u /comfyui/main.py --disable-auto-launch --disable-metadata --verbose "${COMFY_LOG_LEVEL}" --log-stdout &
+    python /comfyui/main.py
 
     # echo "worker-comfyui: Starting RunPod Handler"
     # python -u /handler.py
